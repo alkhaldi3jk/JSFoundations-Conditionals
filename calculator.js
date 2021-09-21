9fee1df8dc17eea305bb05a25a24ab1296d347a9
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-09-21 15:56:11 +0300

    calculator.js

--- calculator.js
+++ calculator.js
@@ -4,3 +4,45 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+//console.log(typeof name)
+
+const firstNumber = parseInt (prompt("please enter the first number:  "));
+//console.log(`${firstNumber}` );
+
+if (firstNumber >=0){
+    console.log(firstNumber);
+}
+else {
+    console.log("wrong input")
+}
+//console.log(typeof firstNmber)
+
+
+const secondNumber = parseInt (prompt("please enter the second number:  "));
+//console.log(`${secondNumber}` );
+if (secondNumber  >=0) {
+    console.log(secondNumber);
+}
+else{
+    console.log("wrong input");
+}
+//console.log(typeof secondNmber)
+
+const operation = prompt("please choose the operation (+,-,/,*):  ");
+
+if (operation === "+") {
+console.log(firstNumber+secondNumber);
+}
+if (operation === "-") {
+    console.log(firstNumber-secondNumber);
+    }
+if (operation === "*") {
+console.log(firstNumber*secondNumber);
+}
+if (operation === "/") {
+    console.log(firstNumber/secondNumber);
+}
+        
+
+
+
